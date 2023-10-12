fc424e60a4e8cd2bb313159c8af9642913e1da54
longnguyen <lnguy013@gmail.com>
2023-10-12 16:04:48 -0700

    My First App on Android Studio

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        val welcomeMSG = findViewById<TextView>(R.id.welcomeMSG)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        val result = findViewById<TextView>(R.id.result)
+
+        startButton.setOnClickListener{
+            val rand = Random().nextInt(seekBar.progress)+1
+            result.text = rand.toString()
+        }
     }
 }
